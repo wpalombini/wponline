ce76acdd1cfff893c3c3fca2b561f4757579cfaa
Walter Palombini <walterpalombini@gmail.com>
2023-03-16 20:38:47 +1000

    Deploy docker container web

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,20 @@
-import { AppShell, Button } from "ui";
+import { AppShell, Button } from 'ui';
 
-export default function Web() {
+export const getServerSideProps = () => {
+  console.log('process.env.NEXT_PUBLIC_TEST:', process.env.NEXT_PUBLIC_TEST);
+  return {
+    props: {
+      dt: new Date().toString(),
+    },
+  };
+};
+
+export default function Web({ dt }: { dt: string }) {
   return (
     <AppShell>
       <h1>Web</h1>
+      <p>Server side rendered at {dt}</p>
+      <p>Node env is {process.env.NODE_ENV}</p>
       <Button />
     </AppShell>
   );
